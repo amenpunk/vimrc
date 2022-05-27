f2b8c443dcd62d8822b91d76cf1195e6d9800b0c
amenpunk <ondasycircuitos@gmail.com>
2022-05-26 18:13:14 -0600

    first approach 2

--- lua/opts.lua
+++ lua/opts.lua
@@ -59,23 +59,14 @@ require('kanagawa').setup({
 
 require'nvim-treesitter.configs'.setup {
     sync_install = false,
-    ensure_installed = "maintained",
     indent = {
         enable = true
     },
     incremental_selection = {
         enable = true,
-        keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-        },
     },
     highlight = {
         enable = true,
-        disable = { "c", "rust" },
-        additional_vim_regex_highlighting = false,
     },
 }
 
