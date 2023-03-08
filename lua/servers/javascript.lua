cf7d2375242b6dce018dd8c75ab100daa17aabbb
amenpunk <ondasycircuitos@gmail.com>
2023-03-08 17:02:33 -0600

    fix path

--- lua/servers/javascript.lua
+++ lua/servers/javascript.lua
@@ -3,7 +3,7 @@ local dap = require('dap')
 dap.adapters.node2 = {
   type = 'executable';
   command = 'node';
-  args = { os.getenv('home') .. '/DAP/vscode-node-debug2/out/src/nodeDebug.js' };
+  args = { os.getenv('HOME') .. '/DAP/vscode-node-debug2/out/src/nodeDebug.js' };
 }
 
 vim.fn.sign_define('DapBreakpoint', { text = '🐞',texthl='', linehl='', numhl='' })
