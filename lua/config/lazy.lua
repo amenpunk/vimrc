0c987561d5b01fae41fec4dc0915898f795588d3
amenpunk <ondasycircuitos@gmail.com>
2024-04-28 20:54:41 -0600

    some plugins fix :art:

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,10 +12,14 @@ require("config.database")
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "material",
+    } },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,21 +1,24 @@
 return {
-  "cpea2506/one_monokai.nvim",
-  { "ellisonleao/gruvbox.nvim" },
-  "https://gitlab.com/__tpb/monokai-pro.nvim",
-  { "kaicataldo/material.vim", branch = "main" },
-  "felipeagc/fleet-theme-nvim",
-  "navarasu/onedark.nvim",
-  "rebelot/kanagawa.nvim",
-  "bluz71/vim-moonfly-colors",
-  { "nyoom-engineering/oxocarbon.nvim" },
-  "nyngwang/nvimgelion",
+  -- "cpea2506/one_monokai.nvim",
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- "https://gitlab.com/__tpb/monokai-pro.nvim",
+  -- "felipeagc/fleet-theme-nvim",
+  -- "navarasu/onedark.nvim",
+  -- "rebelot/kanagawa.nvim",
+  -- "bluz71/vim-moonfly-colors",
+  -- { "nyoom-engineering/oxocarbon.nvim" },
+  -- "nyngwang/nvimgelion",
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "material",
-      style = "darker",
-      material_theme_style = "darker",
-    },
-  },
+  "kaicataldo/material.vim",
+  branch = "main",
+  lazy = true,
+
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "material",
+  --     style = "darker",
+  --     material_theme_style = "darker",
+  --   },
+  -- },
 }
