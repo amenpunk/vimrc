02304e131e52f678812d9f26aa02f77a37a3d634
amenpunk <ondasycircuitos@gmail.com>
2024-06-02 19:22:22 -0600

    todo: fix snippets enabled by file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,13 +6,9 @@ vim.g.material_theme_style = "darker"
 vim.g.material_terminal_italics = 1
 vim.g.code_action_menu_window_border = "single"
 
-vim.g.dashboard_preview_command = "cat"
-vim.g.dashboard_preview_pipeline = "lolcat"
 -- vim.g.dashboard_preview_file = path to logo file like
 -- ~/.config/nvim/neovim.cat
 -- vim.g.dashboard_preview_file_height = 12
 -- vim.g.dashboard_preview_file_width = 80
 
-vim.cmd([[
-  autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-]])
+-- vim.cmd([[ " autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} }) ]])
