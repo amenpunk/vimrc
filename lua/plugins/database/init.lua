5b7f74fb0de3d4b981a32f07af5e2d44a0e46fc1
amenpunk <ondasycircuitos@gmail.com>
2024-07-04 21:58:22 -0600

    database config

--- lua/plugins/database/init.lua
+++ lua/plugins/database/init.lua
@@ -1,7 +1,38 @@
 return {
-  "tpope/vim-dadbod",
+  "kristijanhusak/vim-dadbod-ui",
   dependencies = {
-    "kristijanhusak/vim-dadbod-completion",
-    "kristijanhusak/vim-dadbod-ui",
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
   },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_winwidth = 35
+    vim.g.db_ui_auto_execute_table_helpers = 1
+    vim.g.db_ui_icons = {
+      expanded = "▾",
+      collapsed = "▸",
+      saved_query = "*",
+      new_query = "+",
+      tables = "~",
+      buffers = "»",
+      connection_ok = "✓",
+      connection_error = "✕",
+    }
+    vim.g.db_ui_show_help = 1
+    vim.g.db_ui_winwidth = 30
+    vim.g.db_ui_table_helpers = {
+      postgres = {
+        select = "SELECT * FROM %s LIMIT 100",
+      },
+      mysql = {
+        select = "SELECT * FROM %s LIMIT 100",
+      },
+    }
+  end,
 }
